dfc52e4af7a2f760263e58033671dfe22baf3800
Shun Lassal <lassal.shun@gmail.com>
2024-11-27 11:09:20 +0100

    :art: 2nd Push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './page/home/home.page';
-import { QuizDetailsPage } from './page/quiz-details/quiz-details.page';
+import { HomePage } from './pages/home/home.page';
+import { QuizDetailsPage } from './pages/quiz-details/quiz-details.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
---
+++ src/app/pages/play/play.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-play',
+  imports: [],
+  templateUrl: './play.page.html',
+  styleUrl: './play.page.scss'
+})
+export class PlayPage {
+
+  // On Init
+  // Fetch les questions du quiz en question avec quiz.Id
+  // Système d'affichage de question (Question 1 > 2)
+  // Quand toutes les questions répondues, alors Résultat
+}
